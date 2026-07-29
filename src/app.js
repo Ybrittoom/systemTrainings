1c12b3e9a78cf29caf8f9fa2cbf0d3fdf2095268
Yago Emanuel B. De Moura <yagoemanuel99@gmail.com>
2026-07-28 21:01:22 -0300

    teste de front end

--- src/app.js
+++ src/app.js
@@ -1,9 +1,19 @@
 import express from "express"
 import authRoutes from "./routes/auth.routes.js"
+import { fileURLToPath } from "url"
+import path from "path";
 
 const app = express();//criando a aplicaçao
 
-app.use(express.json());//sempre que chegar algo ela transforma em objeto json
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = pathth.dirname(__filename)
+
+app.use(
+    express.static(
+        path.join(__dirname, "../public")
+    )
+);//sempre que chegar algo ela transforma em objeto json
 
 app.use(authRoutes);//usando todas as rota do arquivo authRoutes.js
 
