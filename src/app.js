599ea28126a2b993f80ac51b9feaa6d5e9a8cc3a
Yago Emanuel B. De Moura <yagoemanuel99@gmail.com>
2026-09-17 22:24:15 -0300

    atualizaçao de package.json

--- src/app.js
+++ src/app.js
@@ -1,24 +1,37 @@
-import express from "express"
-import authRoutes from "./routes/auth.routes.js"
-import viewRoutes from "./routes/view.routes.js"
-import { fileURLToPath } from "url"
+import express from "express";
+
+import authRoutes from "./routes/auth.routes.js";
+import viewRoutes from "./routes/view.routes.js";
+
+import { fileURLToPath } from "url";
 import path from "path";
 
-const app = express();//criando a aplicaçao
+const app = express();
 
+// criando a aplicação
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename)
+const __dirname = path.dirname(__filename);
 
-app.use(express.json())
+app.use(express.json());
 
+// arquivos públicos: CSS, JS, imagens etc.
 app.use(
     express.static(
         path.join(__dirname, "public")
     )
 );
 
+// página inicial
+app.get("/", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "views", "login.html")
+    );
+});
 
-app.use(viewRoutes)
-app.use("/api/auth", authRoutes);//usando todas as rota do arquivo authRoutes.js
+// rotas das páginas
+app.use(viewRoutes);
+
+// rotas da API
+app.use("/api/auth", authRoutes);
 
 export default app;
